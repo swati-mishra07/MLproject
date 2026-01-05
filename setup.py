656f81af8f8b5c0fa2b556f63b643ad56f97a96f
swati-mishra07 <Aanamishra2407@gmail.com>
2026-01-05 22:52:48 +0530

    first setup commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPNEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+
+    '''This function will return the list of requirements'''
+
+    requirements = []
+
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', "") for req in requirements]
+
+        if HYPNEN_E_DOT in requirements:
+            requirements.remove(HYPNEN_E_DOT)
+
+        return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="aana ",
+    author_email="aanamishra2407@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
